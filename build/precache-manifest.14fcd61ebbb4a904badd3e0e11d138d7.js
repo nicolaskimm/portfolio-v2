255ec002875d405e369bb11fe8247ed6ef19aea0
Joanna Krezolek <j.krezolek@gmail.com>
2019-01-25 10:36:51 +0100

    create new build & add package-lock to gitignore

---
+++ build/precache-manifest.14fcd61ebbb4a904badd3e0e11d138d7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c223da7f7446ca2a85ec930f7b5b17",
+    "url": "/chat_app/static/media/chat.c4c223da.jpg"
+  },
+  {
+    "revision": "bc40571a4a9446e907a7",
+    "url": "/chat_app/static/css/main.33507376.chunk.css"
+  },
+  {
+    "revision": "4a123558efa8c14844ec",
+    "url": "/chat_app/static/js/1.4a123558.chunk.js"
+  },
+  {
+    "revision": "3cba61db3c013ed074ec",
+    "url": "/chat_app/static/js/runtime~main.3cba61db.js"
+  },
+  {
+    "revision": "3811c03c2202fb177b455648d1f1f631",
+    "url": "/chat_app/static/media/portrait2.3811c03c.jpg"
+  },
+  {
+    "revision": "7e5d4b62a17c0376c210fb898621e75d",
+    "url": "/chat_app/static/media/portrait1.7e5d4b62.jpg"
+  },
+  {
+    "revision": "bc40571a4a9446e907a7",
+    "url": "/chat_app/static/js/main.bc40571a.chunk.js"
+  },
+  {
+    "revision": "09cd698f36df5d1c61f9fbc68e37aa7e",
+    "url": "/chat_app/static/media/sudoku.09cd698f.jpg"
+  },
+  {
+    "revision": "2946b0318f307ae9ff4cb7c2090860a2",
+    "url": "/chat_app/static/media/bggapp.2946b031.jpg"
+  },
+  {
+    "revision": "e83214471bb346456a4ab3fac101ad7d",
+    "url": "/chat_app/static/media/budgety.e8321447.jpg"
+  },
+  {
+    "revision": "a00038946c4f3677479cdf07e5639590",
+    "url": "/chat_app/static/media/piggame.a0003894.jpg"
+  },
+  {
+    "revision": "944a7131f6360a86e189db463ac59d99",
+    "url": "/chat_app/static/media/psd.944a7131.jpg"
+  },
+  {
+    "revision": "dc1eaa3a16ec2eb145dd1f0a3adb0b57",
+    "url": "/chat_app/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc5169e11bf51dace52f4c72a6752d69.js"
+  "/chat_app/precache-manifest.14fcd61ebbb4a904badd3e0e11d138d7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/chat_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
